3bf4e1c1ca75578ad53398525262fa529252891e
ChrisHersh <ch5968@cs.ship.edu>
2015-03-20 19:55:45 -0400

    Fixed the indentation for the haskell file

--- haskell.hs
+++ haskell.hs
@@ -1,7 +1,7 @@
 fb x
-		| x `mod` 15 == 0 = show x ++ ": FizzBuzz"
-		| x `mod` 5 == 0 = show x ++ ": Fizz"
-		| x `mod` 3 == 0 = show x ++ ": Buzz"
-		| otherwise = show x ++ ":" -- For consistency
+	| x `mod` 15 == 0 = show x ++ ": FizzBuzz"
+	| x `mod` 5 == 0 = show x ++ ": Fizz"
+	| x `mod` 3 == 0 = show x ++ ": Buzz"
+	| otherwise = show x ++ ":" -- For consistency
 
 main = putStr ( unlines ( map fb [1..100]))
